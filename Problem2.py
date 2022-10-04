47bbac1ea81613e86d57d1cd49be0a87e47425f7
Taka <103984069+Taka0007@users.noreply.github.com>
2022-10-04 14:34:03 +0900

    Problem2

---
+++ Problem2.py
@@ -0,0 +1,21 @@
+fibo = [1,2]
+ans = 2
+i = 2
+
+while True:
+    
+    X = fibo[i-1] + fibo[i-2]
+    fibo.append(X)
+    
+    if fibo[i]>= 4000000:
+        
+        break
+    
+    if fibo[i]%2==0:
+        
+        ans += fibo[i]
+        
+    i += 1
+
+print(ans)
+        
