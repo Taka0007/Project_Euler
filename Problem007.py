9731a60d20c49e0137e8b19e1251291ca0448e14
Taka <103984069+Taka0007@users.noreply.github.com>
2023-02-01 00:59:59 +0900

    Create Problem007.py

---
+++ Problem007.py
@@ -0,0 +1,14 @@
+#高速素因数分解のimport
+from sympy import isprime
+
+prime_list = []
+i = 2
+
+while len(prime_list) <= 10**4 + 2:
+
+    if isprime(i):
+      prime_list.append(i)
+
+    i += 1
+
+print(prime_list[10**4])
